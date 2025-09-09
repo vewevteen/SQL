adca7f17c400859e01aa913d595e1d975093cd80
Ron <58986682+vewevteen@users.noreply.github.com>
2025-09-09 23:40:15 +0900

    Update 4-re.sql

--- 4-re.sql
+++ 4-re.sql
@@ -20,3 +20,77 @@ on ps.product_id = p.product_id
 group by p.category_id
 ) as a
 on c.category_id = a.category_id;
+
+
+-- 실습 1 --
+select *
+from product_sales
+where (sale_id, qty*unit_price) = 
+(select sale_id, MAX(qty*unit_price) unit_price
+from product_sales
+group by sale_id
+order by unit_price 
+desc LIMIT 1
+);
+
+-- 실습 2 --
+select * 
+from products
+where category_id in (
+select category_id
+from products
+where price >= 100000
+group by category_id
+having count(*) >= 2
+);
+
+-- 실습 3 --
+select *
+from products
+where (category_id, price)
+in (
+select category_id, MAX(price)
+from products
+group by category_id
+)
+;
+
+-- 실습 4 --
+select p.product_id, p.product_name, p.category_id, p.price
+from products p
+join (
+select category_id, avg(price) as avg_price
+from products
+group by category_id
+) as ap
+on p.category_id = ap.category_id
+and p.price > ap.avg_price;
+
+-- 실습 5 --
+select c.category_id, c.category_name, tp.total_revenue
+from categories c
+join (
+select p.category_id, SUM(ps.qty * ps.unit_price) as total_revenue
+from products p
+join product_sales ps
+on p.product_id = ps.product_id
+group by p.category_id
+) as tp
+on c.category_id = tp.category_id
+LIMIT 1;
+
+-- 실습 6 --
+select product_id,
+product_name,
+price,
+(select SUM(qty * unit_price) as total_price
+from product_sales ps
+where p.product_id = ps.product_id
+group by product_id)
+as total_price,
+(select MAX(sale_date)
+from product_sales ps
+where p.product_id = ps.product_id
+group by product_id)
+as last_sale_date
+from products p
